41764d3054d4dae8d55d8c13dc24bb9a99d4fab0
SebW3 <sebwoss@gmail.com>
2023-07-13 13:43:34 +0200

    changed code to function

--- metalarchives.py
+++ metalarchives.py
@@ -1,40 +1,42 @@
+# This code changes link to search for active band with exact name (only active bands play)
+# Then it waits for scripts to load and extracts band genre for later use
+# TODO rebuild this "API" to not use user browser
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 import re
 
-driver = webdriver.Firefox()
-band_name = "Necrophobic"
-url = f"https://www.metal-archives.com/search/advanced/searching/bands?bandName={band_name}&exactBandMatch=1&genre=&country=&yearCreationFrom=&yearCreationTo=&bandNotes=&status=1&themes=&location=&bandLabelName=#bands"
+def get_genre(band_name):
+    driver = webdriver.Firefox()
 
-driver.get(url)
-while True:  # waiting for scripts to load
-    page_content = driver.page_source
-    if 'tr class="odd"' in page_content:
-        break
-    time.sleep(1)
-    print("waiting 1 sec "*100)
+    url = f"https://www.metal-archives.com/search/advanced/searching/bands?bandName={band_name}&exactBandMatch=1&genre=&country=&yearCreationFrom=&yearCreationTo=&bandNotes=&status=1&themes=&location=&bandLabelName=#bands"
 
-driver.quit()
+    driver.get(url)
+    while True:  # waiting for scripts to load
+        page_content = driver.page_source
+        if 'tr class="odd"' in page_content:
+            break
+        time.sleep(1)
+        print("waiting 1 sec...")
 
-soup = BeautifulSoup(page_content, 'html.parser')
-soup = soup.decode()
+    driver.quit()
 
-def find_content(text):
-    pattern = r'<tr class="odd"(.*)'
-    match = re.search(pattern, text)
-    if match:
-        content = match.group(1)
-        return content.strip()
+    soup = BeautifulSoup(page_content, 'html.parser')
+    soup = soup.decode()
+
+    def find_content(text):
+        pattern = r'<tr class="odd"(.*)'
+        match = re.search(pattern, text)
+        if match:
+            content = match.group(1)
+            return content.strip()
+        else:
+            return None
+
+    result = find_content(soup)
+    if result:
+        temp = result.split("class")
+        band_genre = temp[2][4:-9]
+        return band_genre
     else:
         return None
-
-result = find_content(soup)
-if result:
-    print("Znaleziono zawartość:", result)
-    temp = result.split("class")
-    print(temp)
-    band_genre = temp[2][4:-9]
-    print(band_genre)
-else:
-    print("Nie znaleziono znacznika 'jest'")
