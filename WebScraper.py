ccda28dc5dcbcdf1059ded44752c805c24c400f1
SebW3 <sebwoss@gmail.com>
2023-07-25 01:07:27 +0200

    changed code in if statements to functions

--- WebScraper.py
+++ WebScraper.py
@@ -23,7 +23,9 @@ class WebScraper:
             return None
 
     def scrape_rockmetal_data(self, specific_event_link=None, num_events=None, ALL=None):
-        if specific_event_link:
+        # functions
+        def f_specific_event_link(specific_event_link):
+            print("downloading specific event data")
             response = requests.get(specific_event_link)
             soup = BeautifulSoup(response.content, "html.parser")
             concert_element = soup.find_all(class_="gigItemIn")
@@ -39,22 +41,38 @@ class WebScraper:
                     continue
                 data.append(element.strip())
             return data
-        elif num_events:
+        def f_num_events(num_events):
             print(f"downloading {num_events} events")
             pass
-        elif ALL:
+
+        def f_ALL():
             print("dowloading all events")
-            response = requests.get("https://www.rockmetal.pl/koncerty.html")
+            links = []
+            response = requests.get("https://www.rockmetal.pl/koncerty.html?view=1&page=1")
             soup = BeautifulSoup(response.content, "html.parser")
             target_links = soup.find_all("a", text="szczegóły")
-            links = []
+
             for link in target_links:
                 links.append(link['href'])
-            print(links)
 
-            # TODO here scrape_specific
+            data_ALL = []
+            i = 0
+            for link in links:
+                print(link)
+                data_ALL.append(f_specific_event_link(link))
 
-            pass
+                i += 1  # temp
+                if i > 3:
+                    break
+
+            return data_ALL
+
+        if specific_event_link:
+            return f_specific_event_link(specific_event_link)
+        elif num_events:
+            return f_num_events(num_events)
+        elif ALL:
+            return f_ALL()
         else:
             print("please specify what to scrape")
             pass
@@ -66,8 +84,8 @@ class WebScraper:
 # Testing here
 rockmetal_scraper = WebScraper("rockmetal")
 #specific_event_data = rockmetal_scraper.scrape_data("https://www.rockmetal.pl/koncerty.html?koncert=56719_D_R_I_")
-specific_event_data = rockmetal_scraper.scrape_data("https://www.rockmetal.pl/koncerty.html?koncert=56842_Summer_Discomfort")
-print(specific_event_data)
+#specific_event_data = rockmetal_scraper.scrape_data("https://www.rockmetal.pl/koncerty.html?koncert=56842_Summer_Discomfort")
+#print(specific_event_data)
 
 scrape_ALL = rockmetal_scraper.scrape_data(ALL=True)
 print(scrape_ALL)
